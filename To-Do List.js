0fe48e9dfa3ea33dbe5de881e3e2376aa4f78ef3
Cody Czora <cczora@gmail.com>
2019-05-14 10:39:05 -0500

    Finished advanced version requirements except sorting by days remaining. Will finish later if time permits.

--- To-Do List.js
+++ To-Do List.js
@@ -1,16 +1,19 @@
+//make a variable to count the number of times addItem has been called so far
+//var alertAddNumber = 0;
+//make an array to hold the numDays values of the newTask alerts that have been added so far
+//var daysArray = [];
+
 function addItem() {	
 	
 	//make variables out of the user's inputs
 	var task = document.getElementById("task").value;
-	var numDays = document.getElementById("daysToComplete").value;
+	var daysToComplete = document.getElementById("daysToComplete").value;
 	
-	//alert the user if the input for numDays isn't right
-	if (numDays < 0) {
-		alert("Sorry, this list is for tasks in the future, not the past. Try again with a positive number of days!");
-		document.forms["attributes"]["numDays"].value = "";
-		document.forms["attributes"]["numDays"].focus();
-		return false;
-	}
+	//get today's date
+	var today = Date.now();
+	
+	//subtract daysToComplete from today to make a numDays value
+	var numDays = Math.round((Date.parse(daysToComplete) - today) / 86400000);
 	
 	//make a new div element and call it newTask
 	var newTask = document.createElement("div");
@@ -18,8 +21,18 @@ function addItem() {
 	
 	//make newTask an alert element and assign a color based on its daysToComplete value:
 	
+	//make a newTaskText variable to hold the variable day or day(s) string
+	var newTaskText = new String();
+	newTaskText.type = "string";
+	
+	//make sure the numDays element isn't negative
+	if(numDays < 0) {
+		alert("Please enter a date greater than 5/15/2019!");
+		return false;
+	}
+	
 	//0-2 daysToComplete -> red
-	if (numDays == 0 || numDays <= 2){
+	else if (numDays == 0 || numDays <= 2){
 		newTask.className = "alert alert-danger alert-dismissable fade show";
 	}
 	
@@ -43,11 +56,47 @@ function addItem() {
 	newTask.appendChild(closeButton);
 	
 	//make the inside text of newTask and insert it after the <a> element
-	var newTaskText = task + " <br> " + numDays + " day(s)";
+	if(numDays == 1){
+		newTaskText = task + " <br> " + numDays + " day";
+	}
+	
+	else {
+		newTaskText = task + " <br> " + numDays + " days";
+	}
 	newTask.insertAdjacentHTML('beforeend', newTaskText);
 	
-	//add the alert to the page!
-	document.getElementById("alertRow").appendChild(newTask);
+	
+	//add the alert to the page
+	document.getElementById("alertContainer").appendChild(newTask);
+	
+	
+	
+	//DEPRECATED: on hold until I figute out how to do this!
+	
+	//alertAddNumber += 1;
+	//daysArray[alertAddNumber - 1] = numDays; //array index values start at 0
+	
+	//is the numDays value of this newTask less than the numDays value of the alert previously added?
+	//for(i = 0; i < daysArray.length; i++) {
+		//if(numDays > daysArray[i]) {
+			//document.getElementById("alertContainer").appendChild(newTask); //add the current newTask AFTER the previous newTask
+			//daysArray.pop();
+			//daysArray.splice(i-1, 0, numDays);
+			//return false;
+		//}
+	
+		//else if(numDays < daysArray[i]) {
+			//document.getElementById("alertContainer").insertBefore(newTask, firstChild); //add the current newTask BEFORE the previous newTask
+			//daysArray.pop();
+			//daysArray.splice(i-1, 0, numDays);
+			//return false;
+		//}
+		
+		//else {
+			//document.getElementById("alertContainer").appendChild(newTask); //add the current newTask to the document
+			//return false;			
+		//}
+	//}
 	
 	return false;
 }
